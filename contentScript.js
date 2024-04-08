769e9fe6ab8ff9849d8d3659d600595cfee710cb
nifra_s <mail.nifras@gmail.com>
2024-04-08 23:25:16 +0530

    modified:   contentScript.js 	modified:   popup.css 	modified:   popup.html

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,10 @@
-(() => async () => {
-  fac = chrome.stuInfo = await document.getElementsByClassName("data-table")[0]
-    .rows;
+(() => {
+  stuInfo = document.getElementsByClassName("data-table")[0].rows;
+
   regNum = stuInfo.item(1).cells[1].innerText;
   fName = stuInfo.item(3).cells[1].innerText;
   level = stuInfo.item(7).cells[1].innerText;
   course = stuInfo.item(9).cells[1].innerText;
-  degree = document.getElementsByClassName("data-table")[0].rows.item(11)
-    .cells[1].textContent;
+  degree = stuInfo.item(11).cells[1].innerText;
   chrome.storage.sync.set({ regNum, fName, level, course, degree });
 })();
