a4594faf356722efd55fbcf34505f21137c13aa1
nifra_s <mail.nifras@gmail.com>
2024-04-08 22:45:59 +0530

    new file:   assets/fac/arts.png 	new file:   assets/fac/arts_edu.png 	new file:   assets/fac/edu.png 	new file:   assets/fac/law.png 	new file:   assets/fac/mgmt.png 	new file:   assets/fac/nur.png 	new file:   assets/fac/sci.png 	new file:   assets/fac/spc.png 	new file:   assets/fac/tech.png 	modified:   contentScript.js 	modified:   popup.css 	modified:   popup.html 	modified:   popup.js

--- contentScript.js
+++ contentScript.js
@@ -1,9 +1,11 @@
-(() => {
-  stuInfo = document.getElementsByClassName("data-table")[0].rows;
+(() => async () => {
+  fac = chrome.stuInfo = await document.getElementsByClassName("data-table")[0]
+    .rows;
   regNum = stuInfo.item(1).cells[1].innerText;
-  fullName = stuInfo.item(3).cells[1].innerText;
+  fName = stuInfo.item(3).cells[1].innerText;
+  level = stuInfo.item(7).cells[1].innerText;
   course = stuInfo.item(9).cells[1].innerText;
-  degree = stuInfo.item(11).cells[1].innerText;
-  chrome.storage.sync.set({ regNum });
-
+  degree = document.getElementsByClassName("data-table")[0].rows.item(11)
+    .cells[1].textContent;
+  chrome.storage.sync.set({ regNum, fName, level, course, degree });
 })();
--- popup.js
+++ popup.js
@@ -1,20 +1,49 @@
 import { getActiveTabURL } from "./utils.js";
 
 
-const viewBookmarks = (stuInfo) => {
-  const bookmarks = document.getElementById("bookmarks");
-  const bookmarkTitleElement = document.createElement("div");
-  bookmarkTitleElement.textContent = stuInfo;
-  bookmarkTitleElement.className = "bookmark-title";
-  bookmarks.appendChild(bookmarkTitleElement);
+const viewBookmarks = (stuInfo, faculty) => {
+  const regNum = document.getElementById("reg_num");
+  const facImg = document.getElementById("fac_img");
+  const fName = document.getElementById("f_name");
+  const level = document.getElementById("level");
+  const course = document.getElementById("course");
+  const degree = document.getElementById("degree");
+
+  const facImgTem = document.createElement("img");
+  const regNumTem = document.createElement("a");
+  const fNameTem = document.createElement("a");
+  const levelTem = document.createElement("a");
+  const courseTem = document.createElement("a");
+  const degreeTem = document.createElement("a");
+
+  regNumTem.textContent = stuInfo.regNum;
+  regNum.appendChild(regNumTem);
+
+  fNameTem.textContent = stuInfo.fName;
+  fName.appendChild(fNameTem);
+
+  levelTem.textContent = stuInfo.level;
+  level.appendChild(levelTem);
+
+  courseTem.textContent = stuInfo.course;
+  course.appendChild(courseTem);
+
+  degreeTem.textContent = stuInfo.degree;
+  degree.appendChild(degreeTem);
+
+  facImgTem.className = "profile-img";
+  facImgTem.src = `./assets/fac/${faculty}.png`;
+  facImgTem.alt = "ProfilePicture";
+  facImg.appendChild(facImgTem);
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
   const activeTab = await getActiveTabURL();
 
   if (activeTab.url.includes("results/result_sheet")) {
+    const fac = activeTab.url.split("/")[3];
     const stuInfo = await chrome.storage.sync.get();
-    viewBookmarks(stuInfo.regNum);
+    viewBookmarks(stuInfo, fac);
   } else {
     const container = document.getElementsByClassName("container")[0];
     container.innerHTML =
