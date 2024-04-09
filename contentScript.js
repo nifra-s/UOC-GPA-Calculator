8c48820d27e168b95364bf0f56ce797715fbcfa4
nifra_s <mail.nifras@gmail.com>
2024-04-09 12:03:18 +0530

    modified:   contentScript.js 	modified:   popup.html 	modified:   popup.js

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,34 @@
 (() => {
   stuInfo = document.getElementsByClassName("data-table")[0].rows;
-
   regNum = stuInfo.item(1).cells[1].innerText;
   fName = stuInfo.item(3).cells[1].innerText;
   level = stuInfo.item(7).cells[1].innerText;
   course = stuInfo.item(9).cells[1].innerText;
   degree = stuInfo.item(11).cells[1].innerText;
-  chrome.storage.sync.set({ regNum, fName, level, course, degree });
+
+  info = { regNum, fName, level, course, degree };
+  totalGPV = 0.0;
+  totalCredit = 0.0;
+
+  for (let i = 1; i < parseInt(level) + 1; i++) {
+    currentLevel = document.getElementsByClassName("data-table")[i].rows;
+    for (let j = 2; j < currentLevel.length - 1; j++) {
+      currentCourse = currentLevel.item(j);
+
+      if (currentCourse.cells[5].innerText === "Standard") {
+        gpv = parseFloat(currentCourse.cells[10].innerText);
+        credit = parseFloat(currentCourse.cells[4].innerText);
+
+        if (isNaN(gpv)) {
+          gpv = 0;
+          credit = 0;
+        }
+
+        totalGPV += gpv * credit;
+        totalCredit += credit;
+      }
+    }
+  }
+  gpa = (totalGPV / totalCredit).toFixed(2);
+  chrome.storage.sync.set({ info, gpa });
 })();
--- popup.js
+++ popup.js
@@ -1,13 +1,14 @@
 import { getActiveTabURL } from "./utils.js";
 
 
-const viewBookmarks = (stuInfo, faculty) => {
+const viewBookmarks = (stuInfo, gpa, faculty) => {
   const regNum = document.getElementById("reg_num");
   const facImg = document.getElementById("fac_img");
   const fName = document.getElementById("f_name");
   const level = document.getElementById("level");
   const course = document.getElementById("course");
   const degree = document.getElementById("degree");
+  const GPA = document.getElementById("gpa");
 
   const facImgTem = document.createElement("img");
   const regNumTem = document.createElement("a");
@@ -15,6 +16,7 @@ const viewBookmarks = (stuInfo, faculty) => {
   const levelTem = document.createElement("a");
   const courseTem = document.createElement("a");
   const degreeTem = document.createElement("a");
+  const GPATem = document.createElement("h1");
 
   regNumTem.textContent = stuInfo.regNum;
   regNum.appendChild(regNumTem);
@@ -31,6 +33,9 @@ const viewBookmarks = (stuInfo, faculty) => {
   degreeTem.textContent = stuInfo.degree;
   degree.appendChild(degreeTem);
 
+  GPATem.textContent = gpa;
+  GPA.appendChild(GPATem);
+
   facImgTem.className = "profile-img";
   facImgTem.src = `./assets/fac/${faculty}.png`;
   facImgTem.alt = "ProfilePicture";
@@ -43,7 +48,7 @@ document.addEventListener("DOMContentLoaded", async () => {
   if (activeTab.url.includes("results/result_sheet")) {
     const fac = activeTab.url.split("/")[3];
     const stuInfo = await chrome.storage.sync.get();
-    viewBookmarks(stuInfo, fac);
+    viewBookmarks(stuInfo.info, stuInfo.gpa, fac);
   } else {
     const container = document.getElementsByClassName("container")[0];
     container.innerHTML =
